07841ddbb0e23bac3873b80d04fa0f30fd0f7bd5
Gijs Stokman <w0lfw1ng911@gmail.com>
2022-06-13 15:07:18 +0200

    start of modular setup with functional navigation

--- feature-album-detail/src/main/java/com/incentro/feature_album_detail/AlbumDetailsFragment.kt
+++ feature-album-detail/src/main/java/com/incentro/feature_album_detail/AlbumDetailsFragment.kt
@@ -1,19 +1,20 @@
-package com.incentro.myphotoalbum
+package com.incentro.feature_album_detail
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import com.incentro.myphotoalbum.databinding.FragmentFirstBinding
+import com.incentro.feature_album_detail.databinding.FragmentAlbumDetailsBinding
 
 /**
- * A simple [Fragment] subclass as the default destination in the navigation.
+ * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class AlbumDetailsFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: FragmentAlbumDetailsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,9 +23,9 @@ class FirstFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = FragmentAlbumDetailsBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -32,8 +33,8 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        binding.buttonSecond.setOnClickListener {
+            findNavController().navigate(Uri.parse("incentro://album-overview/"))
         }
     }
 
--- feature-album-overview/src/main/java/com/incentro/feature_album_overview/AlbumOverviewFragment.kt
+++ feature-album-overview/src/main/java/com/incentro/feature_album_overview/AlbumOverviewFragment.kt
@@ -1,30 +1,25 @@
-package com.incentro.myphotoalbum
+package com.incentro.feature_album_overview
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import com.incentro.myphotoalbum.databinding.FragmentSecondBinding
+import com.incentro.feature_album_overview.databinding.FragmentAlbumOverviewBinding
 
-/**
- * A simple [Fragment] subclass as the second destination in the navigation.
- */
-class SecondFragment : Fragment() {
+class AlbumOverviewFragment : Fragment() {
 
-    private var _binding: FragmentSecondBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    private var _binding: FragmentAlbumOverviewBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        _binding = FragmentAlbumOverviewBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -32,8 +27,8 @@ class SecondFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        binding.buttonFirst.setOnClickListener {
+            findNavController().navigate(Uri.parse("incentro://album-details/"))
         }
     }
 
