c046976ca8f516dcce992ed256aec3d648469668
OG Gissa <w0lfw1ng911@gmail.com>
2022-09-07 16:45:03 +0200

    add ui test dependencies, setup and core ui tests

---
+++ feature-album-detail/src/androidTest/java/com/incentro/feature_album_detail/AlbumDetailsScreenTest.kt
@@ -0,0 +1,17 @@
+package com.incentro.feature_album_detail
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import org.junit.Rule
+import org.junit.Test
+
+class AlbumDetailsScreenTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun photosAreDisplayed() {
+
+    }
+
+}
---
+++ feature-album-overview/src/androidTest/java/com/incentro/feature_album_overview/AlbumOverviewScreenTest.kt
@@ -0,0 +1,17 @@
+package com.incentro.feature_album_overview
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import org.junit.Rule
+import org.junit.Test
+
+class AlbumOverviewScreenTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun albumsAreDisplayed() {
+
+    }
+
+}
