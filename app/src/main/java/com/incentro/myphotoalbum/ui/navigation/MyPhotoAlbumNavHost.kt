f8bca8229a33193a0e0948176e5c8c0d4f54d664
OG Gissa <w0lfw1ng911@gmail.com>
2022-09-13 10:47:39 +0200

    Changed MainActivity and Nav test to Global app test

--- app/src/main/java/com/incentro/myphotoalbum/ui/navigation/MyPhotoAlbumNavHost.kt
+++ app/src/main/java/com/incentro/myphotoalbum/ui/navigation/MyPhotoAlbumNavHost.kt
@@ -2,14 +2,11 @@ package com.incentro.myphotoalbum.ui.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.testTag
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import com.incentro.core_ui.navigation.GlobalDestinations
 import com.incentro.feature_album_detail.ui.navigation.albumDetailsGraph
 import com.incentro.feature_album_overview.ui.navigation.albumOverviewGraph
-
-const val APP_NAV_GRAPH_TEST_TAG = "app_nav_graph_test_tag"
 
 @Composable
 fun MyPhotoAlbumNavHost(
@@ -21,7 +18,6 @@ fun MyPhotoAlbumNavHost(
         navController = navController,
         startDestination = GlobalDestinations.FeatureAlbumOverview.route,
         modifier = modifier
-            .testTag(APP_NAV_GRAPH_TEST_TAG)
     ) {
         albumOverviewGraph(navigateTo, modifier)
         albumDetailsGraph(modifier)
