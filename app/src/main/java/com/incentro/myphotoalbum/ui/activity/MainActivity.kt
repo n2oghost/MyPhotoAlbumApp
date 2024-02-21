f89d50203877751a240414a233439c47749a1027
OG Gissa <w0lfw1ng911@gmail.com>
2024-02-21 19:50:10 +0100

    add splash screen

--- app/src/main/java/com/incentro/myphotoalbum/ui/activity/MainActivity.kt
+++ app/src/main/java/com/incentro/myphotoalbum/ui/activity/MainActivity.kt
@@ -3,11 +3,14 @@ package com.incentro.myphotoalbum.ui.activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.incentro.myphotoalbum.ui.composable.AppContent
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+
         super.onCreate(savedInstanceState)
 
         setContent {
